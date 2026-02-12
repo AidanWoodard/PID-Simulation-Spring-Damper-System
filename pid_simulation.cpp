42eabf0f3a7a3ec081a0f93c0ac8d0c81e2e06cb
Aidan <aidanwoodard18@gmail.com>
2026-02-12 00:45:01 -0600

    sbox: practice with file pointer positioning and .read()

--- pid_simulation.cpp
+++ pid_simulation.cpp
@@ -3,17 +3,26 @@
 using namespace std;
 
 int main() {
-  // Create and open a text file
-  fstream MyFile("testFile.txt");
-
-  // Write to the file
-  MyFile << "Hell world.";
+  // prviously created
+  fstream TestFile("testFile.txt");
+  TestFile << "Hell world.";
   
-  // Read from the file
-  string myText;
-  getline(MyFile, myText);
-  cout << myText;
+  // seekg takes (change, origin) such that if our file pointer is at 3, 
+  // File.seekg(-1, fstream::curr) would move the pointer back one char.
+  // note: 'beg' for origin, 'curr' for current pos, and 'end' for end.
+  TestFile.seekg(0, fstream::beg);
+  char contents[4];
+  TestFile.read(contents, 3);
+  contents[3] = '\0';
+  cout << contents << '\n';
 
-  // Close the file
-  MyFile.close();
+  // since we don't move the file pointer, we pick up where we left off.
+  // in this case we start at the second 'l' of Hell world and print 'l w'.
+  char contents2[4];
+  TestFile.read(contents2, 3);
+  contents2[3] = '\0';
+  cout << contents2 << '\n';
+
+  TestFile.close();
+  return 0;
 } 
